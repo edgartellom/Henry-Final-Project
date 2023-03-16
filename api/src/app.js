956730addf8fbca1cd0dee89dd5ce3958210b1fa
edgartellom <edgar.tello@tecsup.edu.pe>
2023-03-15 21:26:04 -0300

    fixed request locahost

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res) => {
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
@@ -24,6 +24,7 @@ server.use((req, res) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
 });
 
 server.use("/", routes);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_API || "http://localhost:3001";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
