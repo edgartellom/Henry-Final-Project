482ddc10967752dce3f0eaa2eb05982fdf5bb5c6
edgartellom <edgar.tello@tecsup.edu.pe>
2023-03-20 21:27:15 -0300

    format front project

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -6,42 +6,79 @@ const Contact = () => {
   return (
     <>
       <Navbar></Navbar>
-      <hr/>
+      <hr />
       <section aria-label="contact us" className="container">
-        <div className="row center" >
+        <div className="row center">
           <div className="row">
-            <article >
-                <hgroup>
-                  <h2>Contact Us</h2>
-                  <h3>Litora torquent per conubia nostra</h3>
-                </hgroup>
-                
-                <aside>
-                  <summary>Meet us</summary>
-                  <ol>
-                    <li><i class="bi bi-geo-alt-fill"></i> Address : Jr. anda la osa 786 Lima - Peru </li>
-                    <li><i class="bi bi-whatsapp"></i> Phones : 5192328522 12354666 316521 </li>
-                    <li><i class="bi bi-envelope-at"></i> Emails: pkito@mail.com</li>
+            <article>
+              <hgroup>
+                <h2>Contact Us</h2>
+                <h3>Litora torquent per conubia nostra</h3>
+              </hgroup>
 
-                  </ol>
-                  
-
-                </aside>
-                <aside>
-                  <summary>Follow us</summary>
-                  <ol>
-                    <li> <NavLink><i class="bi bi-facebook"></i></NavLink></li>
-                    <li> <NavLink><i class="bi bi-twitter"></i></NavLink></li>
-                    <li> <NavLink><i class="bi bi-instagram"></i></NavLink></li>
-                    <li> <NavLink><i class="bi bi-linkedin"></i></NavLink></li>
-                  </ol>
-                  
-
-                </aside>
+              <aside>
+                <summary>Meet us</summary>
+                <ol>
+                  <li>
+                    <i className="bi bi-geo-alt-fill"></i> Address : Jr. anda la
+                    osa 786 Lima - Peru{" "}
+                  </li>
+                  <li>
+                    <i className="bi bi-whatsapp"></i> Phones : 5192328522
+                    12354666 316521{" "}
+                  </li>
+                  <li>
+                    <i className="bi bi-envelope-at"></i> Emails: pkito@mail.com
+                  </li>
+                </ol>
+              </aside>
+              <aside>
+                <summary>Follow us</summary>
+                <ol>
+                  <li>
+                    {" "}
+                    <NavLink>
+                      <i className="bi bi-facebook"></i>
+                    </NavLink>
+                  </li>
+                  <li>
+                    {" "}
+                    <NavLink>
+                      <i className="bi bi-twitter"></i>
+                    </NavLink>
+                  </li>
+                  <li>
+                    {" "}
+                    <NavLink>
+                      <i className="bi bi-instagram"></i>
+                    </NavLink>
+                  </li>
+                  <li>
+                    {" "}
+                    <NavLink>
+                      <i className="bi bi-linkedin"></i>
+                    </NavLink>
+                  </li>
+                </ol>
+              </aside>
               <form className="grid">
-                <input type="input" id="firstname" name="firstname" placeholder="First name" aria-label="First name" required />
-                <input type="email" id="email" name="email" placeholder="Email address" aria-label="Email address" required />
-                <button type="submit" >Subscribe</button>
+                <input
+                  type="input"
+                  id="firstname"
+                  name="firstname"
+                  placeholder="First name"
+                  aria-label="First name"
+                  required
+                />
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email address"
+                  aria-label="Email address"
+                  required
+                />
+                <button type="submit">Subscribe</button>
               </form>
             </article>
           </div>
--- client/src/store/commons.js
+++ client/src/store/commons.js
@@ -1,13 +1,13 @@
-import {create} from "zustand";
-import {devtools} from "zustand/middleware";
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
-const useCommonStore = create(devtools((set)=>({
+const useCommonStore = create(
+  devtools((set) => ({
     theme: false,
     title: "Page",
-    changeTheme: () =>set((state)=>({theme: !state.theme})),
-    changeTitle: (title) =>set({title: title}),
-})))
-
-
+    changeTheme: () => set((state) => ({ theme: !state.theme })),
+    changeTitle: (title) => set({ title: title }),
+  }))
+);
 
 export default useCommonStore;
