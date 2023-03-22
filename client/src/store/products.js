313da44bddbf765bf9963edcd044b4defe3a1cdd
Diego <diego-bonzo@hotmail.com>
2023-03-22 20:51:09 -0300

    filtrados

--- client/src/store/products.js
+++ client/src/store/products.js
@@ -1,14 +1,43 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const useStore = create((set) => ({
+const useStore = create((set, get) => ({
   products: [],
+  listProducts: [],
+  categoryFilter: "",
+  brandFilter: "",
   fetchProducts: async () => {
     try {
       const response = await axios.get("/products");
-      set({ products: response.data });
+      set({ products: response.data, listProducts: response.data });
     } catch (error) {
       console.error(error);
+    }
+  },
+
+  setBrandFilter: (brand) => set({ brandFilter: brand }),
+  getFilteredByBrand: () => {
+    const { listProducts, brandFilter } = get();
+    if (brandFilter !== "") {
+      const filteredByBand = listProducts.filter(
+        (product) => product.brand.toLowerCase() == brandFilter.toLowerCase()
+      );
+      return filteredByBand;
+    } else {
+      return listProducts;
+    }
+  },
+
+  setCategoryFilter: (category) => set({ categoryFilter: category }),
+  getFilteredProducts: () => {
+    const { listProducts, categoryFilter } = get();
+    if (categoryFilter !== "") {
+      const productsFiltered = listProducts.filter((product) =>
+        product.categories.some((cat) => cat.name === categoryFilter)
+      );
+      return productsFiltered;
+    } else {
+      return listProducts;
     }
   },
 }));
