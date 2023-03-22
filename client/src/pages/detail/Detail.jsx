94ee62e359a4575f9deeead616ebfe43ed9ac787
mgongora07 <>
2023-03-21 21:06:31 -0600

    Detalles Productos

--- client/src/pages/detail/Detail.jsx
+++ client/src/pages/detail/Detail.jsx
@@ -1,7 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { Footer, Navbar } from "../../components";
+import {useEffect} from "react";
+import { useParams } from "react-router-dom";
+import useStore from "../../store/products"
 
 const Detail = () => {
+  const fetchProducts = useStore((state) => state.fetchProducts);
+  const products = useStore((state) => state.products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+
+  const{id}=useParams()
+  console.log(id)
+
+
+
+var dataRecibe=id?products.filter((el)=>el.id===id):""
+var data=dataRecibe[0]
+console.log(data)
+
+
+
   return (
     <>
       <Navbar></Navbar>
@@ -11,14 +33,14 @@ const Detail = () => {
             <div className="images">
               <img
                 className="img-fluid"
-                src="https://i0.wp.com/store.teslards.pe/wp-content/uploads/2023/03/r19vi1qiqo3trbcu_setting_xxx_0_90_end_800.png"
+                src={data.image[0]}
               />
             </div>
           </div>
           <div className="col">
             <hgroup>
-              <h3>Model : MONITOR ASUS VG27VH1B 27″ VA FHD</h3>
-              <h4>SKU: 453532121565</h4>
+              <h3>{data.brand}</h3>
+              <h4>{data.model}</h4>
             </hgroup>
             <div className="rate">
               <i className="bi bi-star"></i>
@@ -27,36 +49,22 @@ const Detail = () => {
               <i className="bi bi-star"></i>
             </div>
             <p>
-              desc: Monitor Gamer Curvo TUF Gaming VG27VH1B – 27 pulgadas Full
-              HD (1920×1080), 165Hz (por encima de 144Hz), Extreme Low Motion
-              Blur™, Adaptive-sync, FreeSync™ Premium, 1 ms (MPRT){" "}
+              {data.feature}
             </p>
             <details>
               <summary>Details</summary>
               <ul>
                 <li>
-                  Monitor gamer de 27 pulgadas Full HD (1920×1080) 1500R con
-                  frecuencia de actualización ultrarrápida de 165Hz diseñada
-                  para jugadores profesionales y un juego inmersivo
-                </li>
-                <li>
-                  La tecnología ASUS Extreme Low Motion Blur (ELMB™) permite un
-                  tiempo de respuesta de 1ms (MPRT) junto con sincronización
-                  adaptativa, eliminando el efecto fantasma y el desgarro para
-                  imágenes nítidas de juegos con altas velocidades de cuadros.
-                </li>
-                <li>
-                  Tecnología FreeSync™ Premium para eliminar el desgarro de la
-                  pantalla y las velocidades de fotogramas entrecortadas.
+                 {data.detail}
                 </li>
               </ul>
             </details>
             <hgroup>
               <h5>Categories</h5>
-              <h6>desktop monitores Asus Gaminig</h6>
+              <h6></h6>
             </hgroup>
             <p>
-              Price: <strong>S/ 782.00</strong>
+              Price: <strong>{data.price}</strong>
             </p>
             <div className="actions">
               <div className="btn-inline">
