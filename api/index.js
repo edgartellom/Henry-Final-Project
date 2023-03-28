76cc005c744d3cb860f6efa85d764e81391c953b
Diego <diego-bonzo@hotmail.com>
2023-03-28 16:47:13 -0300

    indexEnFalse

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { conn } = require("./src/db");
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, async () => {
     await getAllCategories();
     await getAllProducts();
