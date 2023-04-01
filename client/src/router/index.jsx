b32cb625809663f6c69d52fc5ad93a0a14a51ea5
Leguiza Gonzalo <leguizagonzalo@gmail.com>
2023-04-01 14:31:07 -0300

    Pasarela de pago - Mercado pago

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home, Login, Products, Detail, Contact } from "../pages";
+import Pending from "../components/paymentState/Pending";
+import Success from "../components/paymentState/Success";
+import Failure from "../components/paymentState/Failure";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,18 @@ const router = createBrowserRouter([
   {
     path: "/contact",
     element: <Contact />,
+  },
+  {
+    path: "/failure",
+    element: <Failure />,
+  },
+  {
+    path: "/pending",
+    element: <Pending />,
+  },
+  {
+    path: "/success",
+    element: <Success />,
   },
 ]);
 
