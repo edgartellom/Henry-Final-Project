8bce90bd4a4ba672893493c079297618386dab51
Leguiza Gonzalo <leguizagonzalo@gmail.com>
2023-04-16 17:38:12 -0300

    CssResponsive

--- client/src/pages/detail/Detail.jsx
+++ client/src/pages/detail/Detail.jsx
@@ -37,53 +37,53 @@ const Detail = () => {
     <>
       <Navbar></Navbar>
 
-      <main className="container">
+      <div className="contenedor">
         {loadingInProgress ? (
           <div className="loader-container">
             <ClipLoader color={"#fff"} loading={loadingInProgress} size={150} />
           </div>
         ) : (
           <div className="row">
-            <div className="col">
-              <div className="images">
-                <img className="img-fluid" src={detailProduct.image} />
+            <div className="x">
+
+          
+                <div className="contenedor-img">
+                  <img className="img-fluid" src={detailProduct.image} />
+                </div>
+            
+            <div className="contenedor-detalles">
+                 <h3 className="nameProduct">{detailProduct.feature}</h3>
+              <div className="cont-det-pro">
+                 <h4 className="detalles-product">Brand: {detailProduct.brand}</h4>
+                 <h4 className="detalles-product">Model: {detailProduct.model}</h4>
               </div>
-            </div>
-            <div className="col">
-              <hgroup>
-                <h3>{detailProduct.brand}</h3>
-                <h4>{detailProduct.model}</h4>
-              </hgroup>
               <div>
                 <i className="bi bi-star"></i>
                 <i className="bi bi-star"></i>
                 <i className="bi bi-star"></i>
                 <i className="bi bi-star"></i>
               </div>
-              <p>{detailProduct.feature}</p>
+              
               <details>
                 <summary>Details</summary>
                 <ul>
-                  <li>{detailProduct.detail}</li>
+                  <li className="detail-product">{detailProduct.detail}</li>
                 </ul>
               </details>
+            
+              </div>
+              </div>
+              <div id="parte-pago">
               <hgroup>
                 <h5>Categories</h5>
                 <li>{detailProduct.categories}</li>
               </hgroup>
-              <p>
+              <p className="price">
                 Price: <strong>{formatter.format(detailProduct.price)}</strong>
               </p>
               <div className="actions">
                 <div className="btn-inline">
-                  <NavLink
-                    to="/cart"
-                    role="button"
-                    className="primary"
-                    data-tooltip="Add to Cart"
-                  >
-                    <i className="bi bi-cart-plus"></i>
-                  </NavLink>
+                 
                   <NavLink
                     to="/detail"
                     role="button"
@@ -92,12 +92,21 @@ const Detail = () => {
                   >
                     <i className="bi bi-heart"></i>
                   </NavLink>
+                  <NavLink
+                    to="/cart"
+                    role="button"
+                    className="primary"
+                    data-tooltip="Add to Cart"
+                  >
+                    <i className="bi bi-cart-plus"></i>
+                  </NavLink>
                 </div>
               </div>
+              </div>
             </div>
-          </div>
+         
         )}
-      </main>
+      </div>
 
       <Footer></Footer>
     </>
